96c8c16371c3d0fcfe9c374d0e59e5462873f08d
fangqian <qiangu_fang@163.com>
2016-04-23 01:12:30 +0800

    更改数据库引擎为mysql

--- shopsys/settings.py
+++ shopsys/settings.py
@@ -76,8 +76,15 @@ WSGI_APPLICATION = 'shopsys.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shopsys',
+        'USER': 'shopsys',
+        'PASSWORD': '12345678',
+
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'TEST': {}
     }
 }
 
@@ -86,6 +93,8 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+
+DATE_FORMAT = 'Y-m-d'
 
 TIME_ZONE = 'UTC'
 
